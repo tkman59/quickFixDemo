19dec36a023953e1064d23605dc682eeb38ed456
tkman <tkman59@gmail.com>
2015-02-08 16:38:40 +0100

    display service + begin modal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,7 +14,7 @@ angular.module('starter.controllers', [])
 
     })
 
-.controller('SearchCtrl', function($scope,$localStorage,$log,$translate,Categories) {
+.controller('SearchCtrl', function($scope,$localStorage,$log,$translate,$ionicModal ,Categories) {
       $scope.isLogin=$localStorage.get('isLogin');
 
       $scope.changeIsLogin = function (){
@@ -32,6 +32,33 @@ angular.module('starter.controllers', [])
 
         $scope.categories = Categories.all();
 
+        $scope.subCategorySelected = $scope.categories[1].subCategories[1];
+
+        $scope.modal = $ionicModal;
+        $ionicModal.fromTemplateUrl('modal-info.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+        $scope.openModal = function() {
+            $scope.modal.show();
+        };
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+        };
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function() {
+            $scope.modal.remove();
+        });
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function() {
+            // Execute action
+        });
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function() {
+            // Execute action
+        });
 
 
 })
